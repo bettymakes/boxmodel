c090151eb5e215776dc489025c1b002ca02e4dea
blibotics <blibotics@gmail.com>
2014-04-30 23:23:01 -0400

    Prefilling inputs with default values, creating prop object to hold all default values

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var app = angular.module('boxmodelApp', []);
 
 app.controller('BoxmodelCtrl', function($scope){
-    $scope.testing = "hello there. I'm angular";
-    console.log('fired');
+    $scope.prop = {
+        elementWidth: 400,
+        elementHeight: 200,
+        paddingTop: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
+        paddingLeft: 20
+    };
 });

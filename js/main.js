01ed385bea6ac414d51b77c27cbb2acdce73f7c6
blibotics <blibotics@gmail.com>
2014-04-30 23:31:00 -0400

    Adding reset button and setting border & margin properties

--- js/main.js
+++ js/main.js
@@ -17,4 +17,22 @@ app.controller('BoxmodelCtrl', function($scope){
         marginBottom: 20,
         marginLeft: 20
     };
+    $scope.setDefault = function(){
+        $scope.prop = {
+            elementWidth: 400,
+            elementHeight: 200,
+            paddingTop: 20,
+            paddingRight: 20,
+            paddingBottom: 20,
+            paddingLeft: 20,
+            borderTop: 20,
+            borderRight: 20,
+            borderBottom: 20,
+            borderLeft: 20,
+            marginTop: 20,
+            marginRight: 20,
+            marginBottom: 20,
+            marginLeft: 20
+        };
+    };
 });
